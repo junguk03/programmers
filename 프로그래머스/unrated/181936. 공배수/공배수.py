d0104efc538abea2f9a55ce1567c171aa9e647d8
junguk03 <97594871+junguk03@users.noreply.github.com>
2023-10-19 21:28:55 +0900

    [level unrated] Title: 공배수, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181936. 공배수/공배수.py
+++ 프로그래머스/unrated/181936. 공배수/공배수.py
@@ -1,9 +1,2 @@
 def solution(number, n, m):
-    max_num = max(n,m)
-    min_num = min(n,m)
-    if max_num%min_num == 0:
-        result = int(not(number%max_num))
-    else:
-        result = int(not(number%(n*m)))
-        
-    return result
+    return int(number % n == 0 and number % m == 0)
