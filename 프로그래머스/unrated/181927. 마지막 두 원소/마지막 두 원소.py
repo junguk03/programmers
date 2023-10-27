82f569a9db88f61f9467978c32f8b89a55b7df4b
junguk03 <97594871+junguk03@users.noreply.github.com>
2023-10-27 15:04:29 +0900

    [level unrated] Title: 마지막 두 원소, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181927. 마지막 두 원소/마지막 두 원소.py
+++ 프로그래머스/unrated/181927. 마지막 두 원소/마지막 두 원소.py
@@ -1,7 +1,6 @@
 def solution(num_list):
-    strlen = len(num_list)
-    if num_list[strlen-2] < num_list[strlen-1]:
-        num_list.append(num_list[strlen-1] - num_list[strlen-2])
+    if num_list[-2] < num_list[-1]:
+        num_list.append(num_list[-1] - num_list[-2])
     else:
-        num_list.append(num_list[strlen-1]*2)
+        num_list.append(num_list[-1]*2)
     return num_list
