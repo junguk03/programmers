7ec1723ca4ec4e5c38d7fc6f726f5ca2db4fed5f
junguk03 <97594871+junguk03@users.noreply.github.com>
2023-11-06 16:52:16 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.py
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.py
@@ -1,12 +1,6 @@
 def solution(t, p):
-    arr = list()
     sum = 0
     for i in range(len(t)-len(p)+1):
-        arr1 = ""
-        for j in range(len(p)):
-            arr1 += t[i+j]
-        arr.append(arr1)
-    for i in range(len(arr)):
-        if arr[i] <= p:
+        if t[i:i+len(p)] <= p:
             sum += 1
     return sum
