80802fd65ea295575fe7f4f9ec574215a4486ae8
junguk03 <97594871+junguk03@users.noreply.github.com>
2024-01-11 19:19:31 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
@@ -1,10 +1,8 @@
 def solution(food):
-    num = len(food) - 1
-    str1 = list()
-    arr = list()
-    for i in range(num):
-        for j in range(food[i+1] // 2):
-            str1.append(i+1)
-    arr = str1 + str1[::-1]
-    arr.insert(len(arr)//2,0)
-    return "".join(map(str,arr))
+    arr = "0"
+    for i in range(len(food) - 1, 0, -1):
+        c = food[i]//2
+        while(c > 0):
+            arr = str(i) + arr + str(i)
+            c -= 1
+    return arr
