70338b76688195fa25a0d7ff3c5d0a7b5904fa66
junguk03 <97594871+junguk03@users.noreply.github.com>
2024-12-04 23:30:13 +0900

    [level 2] Title: 괄호 회전하기, Time: 327.73 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
@@ -1,21 +1,17 @@
 def solution(s):
     count = 0
-    i = 0
     for i in range(len(s)):
         stack = []
         for j in s:
             if not stack:
                 stack.append(j)
                 continue
-            if stack[-1] == '[' and j == ']':
-                stack.pop()
-            elif stack[-1] == '{' and j == '}':
-                stack.pop()
-            elif stack[-1] == '(' and j == ')':
+            if stack[-1] + j == "()" or stack[-1] + j == "[]" or stack[-1] + j == "{}":
                 stack.pop()
             else:
                 stack.append(j)
-        s = s[1:] + s[0]
         if not stack:
             count += 1
+        s = s[1:] + s[0]
+            
     return count
