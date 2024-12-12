128757cf338b210991d52f27608e1b566f1b46fd
junguk03 <97594871+junguk03@users.noreply.github.com>
2024-12-12 22:02:51 +0900

    [level 2] Title: 피보나치 수, Time: 561.10 ms, Memory: 439 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
@@ -1,7 +1,5 @@
-def solution(n):
-    dic = {0:0,1:1}
-    for i in range(1,n):
-        sum = dic[i]+dic[i-1]
-        dic[i+1] = sum
-    answer = dic[n]%1234567
-    return answer
+def solution(num):
+    answer=[0,1]
+    for i in range(2,num+1):
+        answer.append(answer[i-1]+answer[i-2])
+    return answer[-1]%1234567
