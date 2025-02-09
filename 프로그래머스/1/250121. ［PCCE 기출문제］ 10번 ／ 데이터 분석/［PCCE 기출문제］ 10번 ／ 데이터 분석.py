4fbb1e23db35664a3e65c129e607bd5ddeb3a165
junguk03 <97594871+junguk03@users.noreply.github.com>
2025-02-09 22:44:21 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 데이터 분석, Time: 0.23 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.py
+++ 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.py
@@ -1,26 +1,10 @@
 def solution(data, ext, val_ext, sort_by):
     result = []
-    if ext == "code":
-        num1 = 0
-    elif ext == "date":
-        num1 = 1
-    elif ext == "maximum":
-        num1 = 2
-    else:
-        num1 = 3
-        
-    if sort_by == "code":
-        num2 = 0
-    elif sort_by == "date":
-        num2 = 1
-    elif sort_by == "maximum":
-        num2 = 2
-    else:
-        num2 = 3
+    by = ["code", "date", "maximum", "remain"]
         
     for i in range(len(data)):
-        if data[i][num1] < val_ext:
+        if data[i][by.index(ext)] < val_ext:
             result.append(data[i])
     
-    result.sort(key = lambda x:x[num2])
+    result.sort(key = lambda x:x[by.index(sort_by)])
     return result
